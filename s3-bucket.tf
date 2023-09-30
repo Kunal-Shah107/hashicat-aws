8791f2178600ce65809f4df8ba013126d664abea
Kunal-Shah107 <therawknight@gmail.com>
2023-09-30 12:35:34 +0000

    version updated

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,9 +1,7 @@
 module "s3-bucket" {
   source  = "app.terraform.io/huconew/s3-bucket/aws"
   version = "2.8.0"
-}
 
-  bucket_prefix = "acloudguy"  
   bucket = "my-s3-bucket"
   acl    = "private"
 
