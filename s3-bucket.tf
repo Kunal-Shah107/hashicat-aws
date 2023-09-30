97ebf17fb1edde3b7cc9e6670b8776597253a92b
Kunal-Shah107 <therawknight@gmail.com>
2023-09-30 12:30:43 +0000

    version updated

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,7 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+module "s3-bucket" {
+  source  = "app.terraform.io/huconew/s3-bucket/aws"
+  version = "2.8.0"
+}
 
   bucket_prefix = "acloudguy"  
   bucket = "my-s3-bucket"
